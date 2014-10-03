204094eb3688cdcb0c57985b0c0201322a3229a4
ss223ck <ss223ck@student.lnu.se>
2014-10-03 20:34:08 +0200

    Half of the tests run

--- 2-1-c-gissa-det-hemliga-talet-master/1DV402.S2.L1C/1DV402.S2.L1C/GuessedNumber.cs
+++ 2-1-c-gissa-det-hemliga-talet-master/1DV402.S2.L1C/1DV402.S2.L1C/GuessedNumber.cs
@@ -8,9 +8,7 @@ namespace _1DV402.S2.L1C
 {
     public struct GuessedNumber
     {
-        //innehåller en gissnings värde
         public int? Number;
-        //innehåller om det var högt lågt osv.. från enum
         public Outcome Outcome;
     }
 }
--- 2-1-c-gissa-det-hemliga-talet-master/1DV402.S2.L1C/1DV402.S2.L1C/SecretNumber.cs
+++ 2-1-c-gissa-det-hemliga-talet-master/1DV402.S2.L1C/1DV402.S2.L1C/SecretNumber.cs
@@ -6,49 +6,35 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L1C
 {
-    enum Outcome { Indefinite, Low, High, Right, NoMoreGuesses, OldGuess}
-    class SecretNumber
+    public enum Outcome { Indefinite, Low, High, Right, NoMoreGuesses, OldGuess}
+    public class SecretNumber
     {
+        private Random randomInstance = new Random();
         private GuessedNumber[] _guessedNumbers;
         private int? _number;
         public const int MaxNumberOfGuesses = 7;
 
+        public SecretNumber()
+        {
+            _guessedNumbers = new GuessedNumber[MaxNumberOfGuesses];
+            Initialize();
+        }
         public bool CanMakeGuess
         {
             get {
-                // lägg till om man gissat rätt
-                    if ((Count < MaxNumberOfGuesses) || )
+                    if ((Count < MaxNumberOfGuesses) || Guess != _number)
                     {
-                        //return false eller true // om den är sant
                         return true;
                     }
                     else
                     {
-                        //return true eller false
                         return false;
                     }
                 }
             private set { }
         }
-        public int Count
-        {
-            get { return ;}
-            private set {}
-        }
-        public int? Guess
-        {
-            get {
-                if (Count == 0)
-                {
-                    return null;
-                }
-                else
-                {
-                    return;
-                }
-                }
-            private set { }
-        }
+        public int Count{get; private set;}
+        public int? Guess {get; private set;}
         public GuessedNumber[] GuessedNumbers
         {
             //refererar till en kopia av _guessedNumbers field
@@ -61,7 +47,7 @@ namespace _1DV402.S2.L1C
         public int? Number
         {
             get {
-                    if (Count < 7)
+                    if (CanMakeGuess)
                     {
                         return null;
                     }
@@ -70,47 +56,50 @@ namespace _1DV402.S2.L1C
                         return _number;
                     }
                 }
-            set { }
+            set { _number = value; }
         }
-        public Outcome Outcome
-        {
-            get {
-                if(Count == 0)
-                {
-                    return Outcome.Indefinite;
-                }
-                else
-                {
-                    return 
-                }
-                }
-            private set {}
-        }
-
+        public Outcome Outcome { get; private set; }
         public void Initialize()
         {
-
+            Array.Clear(_guessedNumbers, 0, MaxNumberOfGuesses);
+            Number = randomInstance.Next(1, 101);
+            Count = 0;
+            Guess = null;
+            Outcome = Outcome.Indefinite;
         }
         public Outcome MakeGuess(int guess)
         {
-            try
+            if (guess < 0 || guess > 100)
             {
-                if (guess < 0 || guess > 100)
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (guess < _number)
+            {
+                Outcome = Outcome.Low;
+            }
+            else if (guess > _number)
+            {
+                Outcome = Outcome.High;
+            }
+            else if (guess == _number)
+            {
+                Outcome = Outcome.Right;
+            }
+            int counting = 0;
+            do
+            {
+                if (guess == _guessedNumbers[counting].Number)
                 {
-                    throw new ArgumentOutOfRangeException();
+                    Outcome = Outcome.OldGuess;
                 }
-
-            }
-            catch (ArgumentOutOfRangeException)
+                counting++;
+            } while (counting < Count);
+            if (Outcome != Outcome.OldGuess)
             {
-
+                Count++;
             }
-
             return Outcome;
         }
-        public SecretNumber()
-        {
-            
-        }
+        
     }
 }
